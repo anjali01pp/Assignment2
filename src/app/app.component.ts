d502f23775ae7ad4c142dcf41dfe1bfcd261bb64
Anjali P P <anjali.p@zerone.training>
2023-09-27 11:50:56 +0530

    Assignment2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -108,7 +108,7 @@ export class AppComponent {
     }
     if (formula.includes('%')){
     
-     //formula = formula.replace(/%(\d+)/g, '* ($1 / 100)')
+     formula = formula.replace(/%(\d+)/g, '* ($1 / 100)')
     // formula = formula.replace(/(\d+)%+/g, function(match, number) {
     //   const num = parseFloat(number);
     //   return (num / Math.pow(100, match.length - number.length)).toString();
